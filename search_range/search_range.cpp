c38ad52c5462ae18d048a3348d359931ca5b5868
shiruizhi <shiruizhi@blued.com>
2021-11-26 18:43:13 +0800

    feat: update

---
+++ search_range/search_range.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+#include <vector>
+
+using std::vector;
+
+vector<int> searchRange(const vector<int> &nums, int target) {
+    return
+}
+
+int main() {
+    auto r = searchRange(vector<int>{1, 2, 3, 3, 3, 4}, 3);
+    std::cout << r[0] << ' ' << r[1] << std::endl;
+    return 0;
+}
